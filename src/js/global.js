c117b8e87165895abcf69d891dc2fce4871ebe80
Nicolas Le Gall <neovov@gmail.com>
2015-12-11 12:23:13 +0900

    Minor various fixes

--- src/js/global.js
+++ src/js/global.js
@@ -109,12 +109,21 @@ $document.ready(function() {
 $document.ready(function() {
 	var timer, $share = $(".share");
 
-	$window.scroll(throttle(function() {
-		$share.addClass("show");
-		window.clearTimeout(timer);
+	function stop() { window.clearTimeout(timer); }
+	function resume() {
 		timer = window.setTimeout(function() {
 			$share.removeClass("show");
 		}, $share.data("fade"));
+	}
+
+	$share.mouseenter(stop);
+	$share.mouseleave(resume);
+	$share.find("a, button").focus(stop).blur(resume);
+
+	$window.scroll(throttle(function() {
+		$share.addClass("show");
+		stop();
+		resume();
 	}));
 });
 
