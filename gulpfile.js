87403cce3e7687e35dcf306a73c9461adb35689b
Nicolas Le Gall <neovov@gmail.com>
2015-12-17 19:46:11 +0900

    Updated the build process

--- gulpfile.js
+++ gulpfile.js
@@ -67,6 +67,7 @@ var
 	paths = {
 		dist: 'dist/',
 
+		partials:  'src/partials',
 		templates: 'src/templates',
 		pages:     'src/pages',
 
@@ -160,7 +161,28 @@ function folders(dir) {
 }
 
 // Configure nunjucks
-nunjucks.nunjucks.configure(['src'], { watch: false, noCache: true });
+nunjucks.nunjucks
+	.configure(['src'], { watch: false, noCache: true })
+	.addFilter('human', function(input) {
+		var date = new Date(input), month;
+
+		switch (date.getMonth()) {
+			case 0:  month = 'janv.'; break;
+			case 1:  month = 'févr.'; break;
+			case 2:  month = 'mars'; break;
+			case 3:  month = 'avri.'; break;
+			case 4:  month = 'mai'; break;
+			case 5:  month = 'juin'; break;
+			case 6:  month = 'juil.'; break;
+			case 7:  month = 'août'; break;
+			case 8:  month = 'sept.'; break;
+			case 9:  month = 'octo.'; break;
+			case 10: month = 'nove.'; break;
+			case 11: month = 'déce.'; break;
+		}
+
+		return date.getDate() + " " + month + " " + date.getFullYear();
+	});
 
 gulp.task('default', function() {
 	sequence('clean', 'build');
@@ -188,22 +210,37 @@ gulp.task('build:html', function() {
 	var streams = merge();
 
 	function build(folder, parent) {
-		var data = {};
+		var data = {}, metadata = {};
 		parent = parent || "";
 
 		try {
-			data = JSON.parse(fs.readFileSync(path.join(paths.pages, parent, folder, folder + '.json')));
+			metadata = JSON.parse(fs.readFileSync(path.join(paths.pages, parent, folder, folder + '.json')));
+
+			// Wrap the data in a prefix if necessary (doesn't exist already)
+			if (!metadata[metadata.template]) {
+				data[metadata.template === 'board' ? 'obsession' : metadata.template] = metadata;
+			} else {
+				data = metadata;
+			}
 		} catch(e) {}
 
-		try {
-			data.content = fs.readFileSync(path.join(paths.pages, parent, folder, folder + '.html'));
-		} catch(e) {}
+		if (metadata.template === "episode") {
+			// Get the episode's content
+			try {
+				data[metadata.template].content = fs.readFileSync(path.join(paths.pages, parent, folder, folder + '.html'));
+			} catch(e) {}
+
+			// Merge the obsession's data with episode's data (in order to properly load the board)
+			try {
+				data.obsession = JSON.parse(fs.readFileSync(path.join(paths.pages, parent, parent.replace('obsessions/', '') + '.json')));
+			} catch(e) {}
+		}
 
 		// Generate the HTML using the metadata and content
-		if (data.template) {
+		if (metadata.template) {
 			streams.add(
 				gulp
-					.src(path.join(paths.templates, data.template + '.html'))
+					.src(path.join(paths.templates, metadata.template + '.html'))
 					.pipe(nunjucks(data))
 					.pipe(rename(path.join(parent, folder, 'index.html')))
 					.pipe(gulp.dest(paths.dist))
@@ -316,8 +353,9 @@ gulp.task('copy:css:svg',     tasks['copy:svg'](paths.css.svg));
 gulp.task('watch', ['build'], function() {
 	livereload.listen();
 
-	gulp.watch(path.join(paths.pages,         '**/*.{html,json}'),     ['build:html']);
+	gulp.watch(path.join(paths.partials,      '**/*.html'),            ['build:html']);
 	gulp.watch(path.join(paths.templates,     '**/*.html'),            ['build:html']);
+	gulp.watch(path.join(paths.pages,         '**/*.{html,json}'),     ['build:html']);
 	gulp.watch(path.join(paths.css.input,     '**/*.{css,scss,sass}'), ['build:css']);
 	gulp.watch(path.join(paths.css.img.input, '**/*.{gif,jpg,png}'),   ['build:css:img']);
 	gulp.watch(path.join(paths.css.svg.input, '**/*.svg'),             ['build:css:svg']);
