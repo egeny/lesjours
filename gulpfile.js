dc292aea4054eca335e26760a3dfc4a041e0189c
Nicolas Le Gall <neovov@gmail.com>
2015-12-26 19:34:42 +0900

    Changed the human filter for dates

--- gulpfile.js
+++ gulpfile.js
@@ -170,18 +170,18 @@ nunjucks.nunjucks
 		var date = new Date(input), month;
 
 		switch (date.getMonth()) {
-			case 0:  month = 'janv.'; break;
-			case 1:  month = 'févr.'; break;
-			case 2:  month = 'mars'; break;
-			case 3:  month = 'avri.'; break;
-			case 4:  month = 'mai'; break;
-			case 5:  month = 'juin'; break;
-			case 6:  month = 'juil.'; break;
-			case 7:  month = 'août'; break;
-			case 8:  month = 'sept.'; break;
-			case 9:  month = 'octo.'; break;
-			case 10: month = 'nove.'; break;
-			case 11: month = 'déce.'; break;
+			case 0:  month = 'janvier';   break;
+			case 1:  month = 'février';   break;
+			case 2:  month = 'mars';      break;
+			case 3:  month = 'avril';     break;
+			case 4:  month = 'mai';       break;
+			case 5:  month = 'juin';      break;
+			case 6:  month = 'juillet';   break;
+			case 7:  month = 'août';      break;
+			case 8:  month = 'septembre'; break;
+			case 9:  month = 'octobre';   break;
+			case 10: month = 'novembre';  break;
+			case 11: month = 'décembre';  break;
 		}
 
 		return date.getDate() + ' ' + month + ' ' + date.getFullYear();
