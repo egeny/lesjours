fce43671f5700d264f6568c6b3c301d0b53591ee
Nicolas Le Gall <neovov@gmail.com>
2016-01-21 17:41:57 +0900

    Renamed abonnement.html to abonnement.php
    Yes, it is weird but trust me, everything is going to be fine. Take a deep breath and don't fight. That's it, breathe.

--- src/pages/abonnement.php
+++ src/pages/abonnement.php
@@ -12,7 +12,7 @@
 		<div class="col full-height">
 			<div class="subscription full-height overflow-auto">
 				<h2 class="mt-8g mb-2g md-ml-1c lg-ml-1c style-meta-larger">Devenir jouriste</h2>
-				<form action="/abonnement.php" method="post" class="mb-2g md-w-6c md-ml-1c relative">
+				<form method="post" class="mb-2g md-w-6c md-ml-1c relative">
 					<fieldset id="formule" class="mb-4g">
 						<legend class="style-meta-large relative">Choisir ma formule</legend>
 						<ul class="plans row text-center">
@@ -129,4 +129,62 @@
 		</div><!-- end of .col -->
 	</div><!-- end of .row -->
 </div><!-- end of .container -->
+{% endblock %}
+
+{% block js %}
+<script>
+	$document.ready(function() {
+		var
+			$form      = $("div.subscription form"),
+			$fieldsets = $form.find("fieldset"),
+			$summary   = $form.find("p.summary"),
+			$progress  = $form.find("div.progress div");
+
+		function updateProgress() {
+			if ($form.is(":valid")) {
+				// Prefer handling this in JS
+				$fieldsets.addClass("valid");
+				$progress.css("height", "100%");
+				return;
+			}
+
+			var height = 0;
+
+			// Prefer using a class instead of the :valid pseudo-selector
+			// So we can set as "invalid" even valid fieldsets (invalid because a previous fieldset isn't valid)
+			$fieldsets.removeClass("valid");
+
+			$fieldsets.each(function() {
+				var $this = $(this);
+				if ($this.is(":valid")) {
+					$this.addClass("valid");
+					height += $this.outerHeight(true);
+				} else {
+					return false;
+				}
+			});
+
+			$progress.height(height);
+		}
+
+		// Update the summary when selecting a plan
+		$fieldsets.eq(0).find("input").change(function() {
+			var
+				selected = $(this).val(),
+				values   = {
+					jouriste: ["Jouriste", 9, "mois"],
+					"jouriste-cash": ["Jouriste cash", 90, "an"],
+					"jouriste-desargente": ["Jouriste désargenté", 5, "mois"]
+				};
+
+			$summary.removeClass("hidden");
+			$summary.find("span").each(function(index) {
+				$(this).text(values[selected][index]);
+			});
+		});
+
+		$form.find("input").change(updateProgress);
+		$window.resize(updateProgress);
+	});
+</script>
 {% endblock %}
