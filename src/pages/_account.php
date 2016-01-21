9317c0d1b679ee945f1f450eb7352a0c19f1e871
Nicolas Le Gall <neovov@gmail.com>
2016-01-21 16:46:51 +0900

    Updated the header to use an iframe for the account
    In this way we keep the static site, just this "partial" if dynamic and not the whole page

---
+++ src/pages/_account.php
@@ -0,0 +1,29 @@
+{% extends "partials/_layout-iframe.html" %}
+
+{% block php -%}
+<?php
+	set_include_path(dirname(__FILE__).'/../../lj');
+	require('wp-load.php');
+	$connected = is_user_logged_in();
+?>
+{% endblock %}
+
+{% block content %}
+	<div class="full-height mh-1m relative">
+		<div id="account">
+		<?php if ($connected) : ?>
+			<a class="btn-round btn-transparent" href="/profile.html">
+				<img class="responsive" src="/img/profile.svg" alt="Accéder à mon profil" />
+			</a>
+		<?php else : ?>
+			<a class="sm-hidden btn-square" href="/abonnement.html">S’abonner</a>
+			{# An anchor link won't open in the parent's frame #}
+			{# We have to set the parent's URL (referer) on the link #}
+			<a class="login" href="<?php echo $_SERVER['HTTP_REFERER'] ?>#login">
+				<i class="md-hidden lg-hidden btn-round btn-transparent">{{ icon("ui-login") }}</i>
+				<span class="sm-sr btn-square">Se connecter</span>
+			</a>
+		<?php endif ?>
+		</div>
+	</div>
+{% endblock %}
