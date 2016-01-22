da612429fda4e344b4175a899f4291e9a2cc5d2e
Nicolas Le Gall <neovov@gmail.com>
2016-01-22 19:32:54 +0900

    Updated the subscription page for data validation

--- src/pages/abonnement.php
+++ src/pages/abonnement.php
@@ -10,6 +10,9 @@
 <?php
 	require('wp.php');
 
+	$data  = $_POST;
+	$error = null;
+
 	$hidden = array(
 		'amount'        => null,
 		'cardfullname'  => null,
@@ -29,8 +32,7 @@
 		'jouriste-desargente' => 5
 	);
 
-	$secret = '<P?[E}D4pRBGl%qO';
-	$state  = null;
+	$state = null;
 
 	$subscriptions = array(
 		'9'  => '+1 month',
@@ -38,70 +40,131 @@
 		'5'  => '+1 month'
 	);
 
+	function signature($array) {
+		$hash = array();
+		$secret = '<P?[E}D4pRBGl%qO'; // FIXME: should be external (const in another file)
+
+		foreach ($array as $name => $value) {
+			$name = strtoupper($name);
+			if ($name == 'HASH') { continue; }
+			$hash[] = $name.'='.$value;
+		}
+
+		sort($hash);
+		return hash('sha256', $secret.implode($secret, $hash).$secret);
+	}
+
+	// Receiving a notification from the payment service
+	if (isset($_GET['notification'])) {
+		// TODO (have to be online)
+		file_put_contents('notification.log', print_r($_GET, true)."\n".print_r($_POST, true));
+	}
+
+	// Receiving a result from the payment service
 	if (isset($_GET['result'])) {
-		// TODO: /!\ check hash
-		if ($_GET['EXECCODE'] == '0000') {
-			$expire  = date('Y-m-d', strtotime($subscriptions[substr($_GET['AMOUNT'], 0, -2)]));
-			$user_id = $_GET['CLIENTIDENT'];
+		$state = 'result';
 
-			// Update the user's account
-			update_user_meta($user_id, 'alias',  $_GET['ALIAS']);
-			update_user_meta($user_id, 'expire', $expire);
-			update_user_meta($user_id, 'paid',   true);
+		if (empty($_GET['result'])) {
+			unset($_GET['result']); // Exclude for the hash computation
+			$hash  = signature($_GET);
 
-			die(header('Location: /merci.html'));
-		} else {
-			echo $_GET['MESSAGE'];
-			die();
+			$error = !$error && $hash != $_GET['HASH']      ? 'hash'            : $error;
+			$error = !$error && $_GET['EXECCODE'] != '0000' ? $_GET['EXECCODE'] : $error;
+
+			if (!$error) {
+				// TODO: should be done in the notification
+				// TODO: should send email (in notification)
+				$expire  = date('Y-m-d', strtotime($subscriptions[substr($_GET['AMOUNT'], 0, -2)]));
+				$user_id = $_GET['CLIENTIDENT'];
+
+				// Update the user's account
+				update_user_meta($user_id, 'alias',  $_GET['ALIAS']);
+				update_user_meta($user_id, 'expire', $expire);
+				update_user_meta($user_id, 'paid',   true);
+
+				// TODO: login
+				wp_set_auth_cookie($user_id, true, false);
+			}
+
+			// Prefer redirecting to remove informations in the URL
+			die(header('Location: /abonnement.html?result='.($error ? $error : 'success')));
 		}
 	}
 
+	// Receiving data from the form
 	if (!empty($_POST)) {
-		// TODO: check inputs
-		// TODO: sanitize
+		// Makes sure there is an "accept" field to check against
+		$data['accept'] = isset($data['accept']) ? $data['accept'] : null;
 
-		// Try to create a new user
-		$user_id = wp_insert_user(array(
-			'user_email' => $_POST['email'],
-			'user_login' => $_POST['email'],
-			'user_pass'  => $_POST['password'],
-			'first_name' => $_POST['firstname'],
-			'last_name'  => $_POST['name']
-		));
+		// Sanitize and check received data
+		foreach ($data as $field => $value) {
+			switch ($field) {
+				case 'email':
+					$value = sanitize_email($_POST[$field]);
+					$value = is_email($value) ? $value : null;
+				break;
 
-		// If everything went fine
-		if (!is_wp_error($user_id)) {
-			// Add additionnal metadata
-			foreach (array('plan', 'address', 'zip', 'city', 'payment') as $field) {
-				add_user_meta($user_id, $field, $_POST[$field], true);
+				case 'payment':
+					$value = sanitize_text_field($_POST[$field]);
+					$value = in_array($value, array('bank', 'card')) ? $value : null;
+				break;
+
+				case 'plan':
+					$value = sanitize_text_field($_POST[$field]);
+					$value = in_array($value, array_keys($prices)) ? $value : null;
+				break;
+
+				case 'password': $value = $_POST[$field];
+				break;
+
+				default: $data[$field] = $value = sanitize_text_field($_POST[$field]);
 			}
 
-			// Mark as "unpaid" for now
-			add_user_meta($user_id, 'paid', false, true);
-
-			// Complete the payload for the payment service
-			$hidden['amount']       = $prices[$_POST['plan']] * 100;
-			$hidden['cardfullname'] = $_POST['name'].' '.$_POST['firstname'];
-			$hidden['clientemail']  = $_POST['email'];
-			$hidden['clientident']  = $user_id;
-			$hidden['orderid']      = date('Y-m-d').'-'.$user_id;
-
-			$hash = array();
-			foreach ($hidden as $name => $value) {
-				$hash[] = strtoupper($name).'='.$value;
+			// Set an error flag if necessary
+			if (empty($value)) {
+				$error = is_array($error) ? $error : array();
+				$error[$field] = true;
 			}
-
-			sort($hash);
-			$hidden['hash'] = $secret.implode($secret, $hash).$secret;
-			$hidden['hash'] = hash('sha256', $hidden['hash']);
-
-			$state = 'redirect';
-		} else {
-			// TODO: Handle existing users
-			print_r($user_id);
-			die();
 		}
-	}
+
+		if (!$error) {
+			// Try to create a new user
+			$user_id = wp_insert_user(array(
+				'user_email' => $data['email'],
+				'user_login' => $data['email'],
+				'user_pass'  => $data['password'],
+				'first_name' => $data['firstname'],
+				'last_name'  => $data['name']
+			));
+
+			$error = is_wp_error($user_id) ? array('account' => $user_id) : null;
+
+			if (!$error) {
+				// Add additionnal metadata
+				foreach (array('plan', 'address', 'zip', 'city', 'payment') as $field) {
+					add_user_meta($user_id, $field, $data[$field], true);
+				}
+
+				// Mark as "unpaid" for now
+				add_user_meta($user_id, 'paid', false, true);
+
+				if ($data['payment'] == 'card') {
+					// Complete the payload for the payment service
+					$hidden['amount']       = $prices[$data['plan']] * 100;
+					$hidden['cardfullname'] = $data['name'].' '.$data['firstname'];
+					$hidden['clientemail']  = $data['email'];
+					$hidden['clientident']  = $user_id;
+					$hidden['orderid']      = date('Y-m-d').'-'.$user_id;
+					$hidden['hash']         = signature($hidden);
+
+					// Generate an hidden form containing the needed informations for the payment service
+					$state = 'redirect';
+				} else {
+					// TODO: bank
+				}
+			}
+		}
+	} // end of if (!empty($_POST))
 ?>
 {% endblock %}
 
@@ -121,7 +184,25 @@
 				<script>
 					document.getElementById("redirect").submit();
 				</script>
+			<?php elseif ($state == 'result') : ?>
+				<?php if ($_GET['result'] == 'success') : ?>
+					<h2 class="mt-8g mb-2g md-ml-1c lg-ml-1c style-meta-larger"><i class="legend-before">{{ icon("check") }}</i>Confirmation d’abonnement</h2>
+					<div class="md-ml-1c lg-ml-1c">
+						<p>Félicitations ! Vous êtes désormais jouriste. <a class="text-upper" href="#">Voir mon profil</a></p>
+						<p>Vous pouvez maintenant naviguer sur le site.</p>
+						<a class="btn-primary" href="/">Voir la une</a>
+					</div>
+				<?php else : ?>
+					<h2 class="mt-8g mb-2g md-ml-1c lg-ml-1c style-meta-larger"><i class="legend-before">{{ icon("check") }}</i>Erreur</h2>
+					<div class="md-ml-1c lg-ml-1c">
+						<p><?php echo $_GET['result'] == 'hash' ? 'bad HASH' : $_GET['result'] ?></p>
+					</div>
+				<?php endif ?>
 			<?php else : ?>
+				<?php
+					// FIXME: errors are disabled for now
+					$error = null;
+				?>
 				<h2 class="mt-8g mb-2g md-ml-1c lg-ml-1c style-meta-larger">Devenir jouriste</h2>
 				<form method="post" class="mb-2g md-w-6c md-ml-1c relative">
 					<fieldset id="formule" class="mb-4g">
@@ -133,7 +214,7 @@
 									<span class="name">Jouriste</span>
 									<span class="desc">Sans engagement de durée</span>
 									<small>2 profils par abonnement</small>
-									<input class="sr" type="radio" name="plan" value="jouriste" required />
+									<input class="sr" type="radio" name="plan" value="jouriste" required <?php if ($data['plan'] == 'jouriste') : ?>checked <?php endif ?>/>
 									<span class="action">Choisir</span>
 								</label>
 							</li>
@@ -143,7 +224,7 @@
 									<span class="name">Jouriste cash</span>
 									<span class="desc">Sans engagement de durée</span>
 									<small>2 profils par abonnement</small>
-									<input class="sr" type="radio" name="plan" value="jouriste-cash" required />
+									<input class="sr" type="radio" name="plan" value="jouriste-cash" required <?php if ($data['plan'] == 'jouriste-cash') : ?>checked <?php endif ?>/>
 									<span class="action">Choisir</span>
 								</label>
 							</li>
@@ -153,7 +234,7 @@
 									<span class="name">Jouriste désargenté</span>
 									<span class="desc">Sans engagement de durée</span>
 									<small>Pour les -25 ans, chômeurs, fauchés, radins</small>
-									<input class="sr" type="radio" name="plan" value="jouriste-desargente" required />
+									<input class="sr" type="radio" name="plan" value="jouriste-desargente" required <?php if ($data['plan'] == 'jouriste-desargente') : ?>checked <?php endif ?>/>
 									<span class="action">Choisir</span>
 								</label>
 							</li>
@@ -167,36 +248,45 @@
 								</a>
 							</li>
 						</ul>
+						<!-- TODO: error message -->
 					</fieldset>
 					<fieldset id="coordonnees" class="lg-w-6c">
 						<legend class="mb-2g style-meta-large relative">Mes coordonnées</legend>
 						<div class="field">
 							<label for="name">Nom</label>
-							<input id="name" class="check md-white-check lg-white-check" name="name" type="text" placeholder="Dupont" autocomplete="family-name" required />
+							<input id="name" class="check md-white-check lg-white-check" name="name" type="text" placeholder="Dupont" autocomplete="family-name" <?php if ($data['name']) { echo 'value="'.$data['name'].'" '; } ?>required />
+							<?php if ($error['name']) : ?><span class="error color-brand">Vérifiez ce champ</span><?php endif ?>
 						</div>
 						<div class="field">
 							<label for="firstname">Prénom</label>
-							<input id="firstname" class="check md-white-check lg-white-check" name="firstname" type="text" placeholder="Jean" autocomplete="given-name" required />
+							<input id="firstname" class="check md-white-check lg-white-check" name="firstname" type="text" placeholder="Jean" autocomplete="given-name" <?php if ($data['firstname']) { echo 'value="'.$data['firstname'].'" '; } ?>required />
+							<?php if ($error['firstname']) : ?><span class="error color-brand">Vérifiez ce champ</span><?php endif ?>
 						</div>
 						<div class="field">
 							<label for="email">Adresse e-mail</label>
-							<input id="email" class="check md-white-check lg-white-check" name="email" type="email" placeholder="mon-email@exemple.com" autocomplete="email" required />
+							<input id="email" class="check md-white-check lg-white-check" name="email" type="email" placeholder="mon-email@exemple.com" autocomplete="email" <?php if ($data['email']) { echo 'value="'.$data['email'].'" '; } ?>required />
+							<?php if ($error['email']) : ?><span class="error color-brand">Vérifiez ce champ</span><?php endif ?>
+							<?php if ($error['account']) : ?><span class="error color-brand">Ce compte existe</span><?php endif ?>
 						</div>
 						<div class="field">
 							<label for="password">Mot de passe</label>
-							<input id="password" class="check md-white-check lg-white-check" name="password" type="password" placeholder="××××××××" autocomplete="new-password" required />
+							<input id="password" class="check md-white-check lg-white-check" name="password" type="password" placeholder="××××××××" autocomplete="new-password" <?php if ($data['password']) { echo 'value="'.$data['password'].'" '; } ?>required />
+							<?php if ($error['password']) : ?><span class="error color-brand">Vérifiez ce champ</span><?php endif ?>
 						</div>
 						<div class="field">
 							<label for="address">Adresse</label>
-							<input id="address" class="check md-white-check lg-white-check" name="address" type="text" placeholder="1 avenue des Champs-Élysées" autocomplete="street-address" required />
+							<input id="address" class="check md-white-check lg-white-check" name="address" type="text" placeholder="1 avenue des Champs-Élysées" autocomplete="street-address" <?php if ($data['address']) { echo 'value="'.$data['address'].'" '; } ?>required />
+							<?php if ($error['address']) : ?><span class="error color-brand">Vérifiez ce champ</span><?php endif ?>
 						</div>
 						<div class="field">
 							<label for="zip">Code postal</label>
-							<input id="zip" class="check md-white-check lg-white-check" name="zip" type="text" minlength="5" maxlength="5" placeholder="75008" autocomplete="postal-code" required />
+							<input id="zip" class="check md-white-check lg-white-check" name="zip" type="text" minlength="5" maxlength="5" placeholder="75008" autocomplete="postal-code" <?php if ($data['zip']) { echo 'value="'.$data['zip'].'" '; } ?>required />
+							<?php if ($error['zip']) : ?><span class="error color-brand">Vérifiez ce champ</span><?php endif ?>
 						</div>
 						<div class="field">
 							<label for="city">Ville</label>
-							<input id="city" class="check md-white-check lg-white-check" name="city" type="text" placeholder="Paris" autocomplete="address-level2" required />
+							<input id="city" class="check md-white-check lg-white-check" name="city" type="text" placeholder="Paris" autocomplete="address-level2" <?php if ($data['city']) { echo 'value="'.$data['city'].'" '; } ?>required />
+							<?php if ($error['city']) : ?><span class="error color-brand">Vérifiez ce champ</span><?php endif ?>
 						</div>
 					</fieldset>
 					<fieldset id="mode-de-paiement">
@@ -207,22 +297,24 @@
 						</div>
 						<div class="field row mb-1g">
 							<label class="col md-w-auto pr-2g pl-0 color-dark">
-								<input class="radio" type="radio" name="payment" value="bank" required disabled>
+								<input class="radio" type="radio" name="payment" value="bank" <?php if ($data['payment'] == 'bank') : ?>checked <?php endif ?>required disabled />
 								<span class="radio"></span>
 								Prélèvement
 							</label>
 							<label class="col md-w-auto pr-2g pl-0">
-								<input class="radio" type="radio" name="payment" value="card" required>
+								<input class="radio" type="radio" name="payment" value="card" <?php if ($data['payment'] == 'card') : ?>checked <?php endif ?>required />
 								<span class="radio"></span>
 								Carte bancaire
 							</label>
+							<!-- TODO: error message -->
 						</div>
 					</fieldset>
 					<div>
 						<label class="mb-2g relative style-meta text-upper">
-							<input class="checkbox" type="checkbox" name="allowed" required>
+							<input class="checkbox" type="checkbox" name="accept" <?php if ($data['accept']) : ?>checked <?php endif ?>required>
 							<span class="checkbox"></span>
 							J’accepte les conditions générales de vente. <a class="color-brand" href="/abonnement-conditions-generales.html">Lire les <abbr title="Conditions Générales de Vente">CGV</abbr></a>.
+							<!-- TODO: error message -->
 						</label>
 						<p class="summary hidden mv-4g pa-2g style-meta lh-inherit text-upper color-main">Vous avez choisi la formule « <span>Jouriste</span> » à <span>9</span> €/<span>mois<span>. <a class="color-brand" href="#formule">Modifier</a></p>
 						<div class="mt-2g relative text-center">
@@ -297,6 +389,7 @@
 
 		$form.find("input").change(updateProgress);
 		$window.resize(updateProgress);
+		updateProgress();
 	});
 </script>
 {% endblock %}
