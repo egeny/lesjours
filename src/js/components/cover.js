f83fadeb54dce589564dcb3801444d06bce0b507
Nicolas Le Gall <neovov@gmail.com>
2016-01-29 12:54:37 +0900

    Updated the components/cover.js to handle vertical swipes

--- src/js/components/cover.js
+++ src/js/components/cover.js
@@ -1,15 +1,51 @@
 $document.ready(function() {
 	"use strict";
 
-	$(".cover .btn").click(function(e) {
-		var $cover = $(this).parents(".cover");
+	var
+		$carousel = $(".carousel"),
+		$covers   = $carousel.find(".cover");
 
-		$cover.toggleClass("expanded");
+	function expand(i) {
+		var $cover = $covers.eq(i);
 
-		if ($cover.parent().hasClass("carousel")) {
-			$cover.parent().data("state", "paused").trigger("state-changed");
+		if ($cover.hasClass("expanded")) {
+			window.location = $cover.find("[href]").attr("href");
+		} else {
+			$cover.addClass("expanded");
+			$carousel.data("state", "paused").trigger("state-changed");
 		}
+	}
 
+	function resume(i) {
+		var $cover = $covers.eq(i);
+
+		if ($cover.hasClass("expanded")) {
+			$cover.removeClass("expanded");
+			$carousel.data("state", "running").trigger("state-changed");
+		}
+	}
+
+	function swiped(e) {
+		var
+			$cover = $covers.filter(".selected"),
+			index  = $cover.length ? $covers.index($cover) : 0;
+
+		if (e.type === "swipedown") {
+			expand(index);
+		} else {
+			resume(index);
+		}
+	}
+
+	$covers.find("[href]").click(function(e) {
+		var $cover = $covers.filter(".selected");
+		expand($cover.length ? $covers.index($cover) : 0);
 		e.preventDefault();
 	});
+
+	$carousel.hammer().bind("swipedown", swiped);
+	$carousel.hammer().bind("swipeup",   swiped);
+
+	// By default Hammer doesn't allow swiping down (for obvious reasons)
+	$carousel.data("hammer").get("swipe").set({ direction: Hammer.DIRECTION_ALL });
 });
