e8c7df157cad4de7a473ec554bbbbed6f18590c5
Nicolas Le Gall <neovov@gmail.com>
2016-01-30 19:15:58 +0900

    Simplified the account script

--- src/pages/_account.php
+++ src/pages/_account.php
@@ -1,35 +1,16 @@
 {% extends "partials/_layout-iframe.html" %}
 
 {% block php -%}
-<?php
-	require('wp.php');
-	$connected = is_user_logged_in();
-
-	// Get the avatar's URL (check the URL first to provide an SVG fallback — not possible with gravatar)
-	if ($connected) {
-		// Simplified version of https://gist.github.com/justinph/5197810
-		$hash = md5(strtolower(trim($current_user->user_email)));
-		$url  = 'http://www.gravatar.com/avatar/'.$hash.'?s=90';
-		$code = wp_cache_get($hash);
-
-		if (!$code) {
-			$response = wp_remote_head($url.'&d=400');
-			$code     = is_wp_error($response) ? '400' : $response['response']['code'];
-			wp_cache_set($hash, $code, null, 60 * 5);
-		}
-
-		$avatar = $code == '200' ? $url : '/img/profile.svg';
-	}
-?>
+<?php require('wp.php'); ?>
 {% endblock %}
 
 {% block content %}
 	<div class="full-height mh-1m relative">
 		<div id="account">
-		<?php if ($connected) : ?>
+		<?php if (is_user_logged_in()) : ?>
 			<a class="btn-round" href="/session?close">{{ icon("logout", "Se déconnecter") }}</a>
 			<a class="btn-round" href="/compte.html">
-				<img class="responsive full-height" src="<?php echo $avatar; ?>" alt="Accéder à mon compte" />
+				<img class="responsive full-height" src="<?php echo avatar_url(); ?>" alt="Accéder à mon compte" />
 			</a>
 		<?php else : ?>
 			<a class="btn-square sm-hidden" href="/abonnement.html">S’abonner</a>
